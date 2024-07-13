cbd8a6efa2cb5dc371a6a1762e771a306419bc66
Joowon Hyun <lkk7913@naver.com>
2024-07-13 15:29:04 +0900

    Initial Commit

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,19 +1,22 @@
 import type { GatsbyConfig } from "gatsby";
+import dotenv from 'dotenv';
+dotenv.config()
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `joowon blog`,
+    title: `joowon's dev`,
     siteUrl: `https://www.yourdomain.tld`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
+  jsxRuntime: 'automatic',
   plugins: [{
     resolve: 'gatsby-source-contentful',
     options: {
-      "accessToken": "w2uCEkbf8x1aXj_7pDNQ7OY9XjA_Lx0Oz5vdQDTJ8YY",
-      "spaceId": "v8jcs71vfccu"
+      accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      spaceId: process.env.CONTENTFUL_SPACE_ID,
     }
   }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-styled-components", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-source-filesystem',
